ea1436ef0f31d97e899d17639796f8a27d5ce13c
Philip Newsham <philip.newsham@hotmail.co.uk>
2017-02-27 21:59:28 +0000

    Build 1

---
+++ Assets/Scripts/ChooseShape.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChooseShape : MonoBehaviour {
+    public GameObject[] shapes;
+    public GameObject currentShape;
+    private int m_currentShapeNo;
+    public Material[] materials;
+    public Material currentMaterial;
+    
+    void Start()
+    {
+        ChooseCurrentShape(0);
+        ChooseCurrentColour(0);
+    }
+    public void ChooseCurrentShape(int shapeNo)
+    {
+        m_currentShapeNo = shapeNo;
+        currentShape = shapes[m_currentShapeNo];
+    }
+
+    public void ChooseCurrentColour(int colourNo)
+    {
+        currentMaterial = materials[colourNo];
+    }
+
+    public Material CurrentMaterial()
+    {
+        return currentMaterial;
+    }
+
+    public GameObject CurrentShape()
+    {
+        return currentShape;
+    }
+
+
+}
---
+++ Assets/Scripts/PlaceShape.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceShape : MonoBehaviour
+{
+    public GameObject shape;
+    private RaycastHit hit;
+    Ray ray;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shape = GetComponent<ChooseShape>().CurrentShape();
+            ray = (Camera.main.ScreenPointToRay(Input.mousePosition));
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 position = new Vector3(hit.transform.position.x, hit.transform.position.y + .3f, hit.transform.position.z);
+                GameObject clone = Instantiate(shape, position, Quaternion.identity) as GameObject;
+                clone.GetComponentInChildren<Renderer>().material = GetComponent<ChooseShape>().CurrentMaterial();
+            }
+        }
+    }
+}
