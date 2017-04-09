5c357b8b10dc0be4427451817feb55522b17106f
Philip Newsham <philip.newsham@hotmail.co.uk>
2017-04-10 00:15:40 +0100

    Added build

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -6,9 +6,13 @@ public class Exit : MonoBehaviour
 {
     public Image[] heartImages;
     private int m_currentPlayer;
+    private float m_heartFill;
+    private float m_totalheartFill;
     void OnTriggerEnter(Collider other)
     {
-        heartImages[m_currentPlayer].fillAmount = other.gameObject.GetComponent<BlockHeadMove>().heartFillAmount;
+        m_heartFill = other.gameObject.GetComponent<BlockHeadMove>().heartFillAmount;
+        heartImages[m_currentPlayer].fillAmount = m_heartFill;
+        m_totalheartFill += m_heartFill;
         m_currentPlayer += 1;
         if(m_currentPlayer == heartImages.Length)
         {
@@ -16,9 +20,12 @@ public class Exit : MonoBehaviour
         }
 
     }
-
+    public Image endHeart;
+    public GameObject endScreen;
     void FinishGame()
     {
+        endScreen.SetActive(true);
+        endHeart.fillAmount = m_totalheartFill / heartImages.Length;
         print("Game Finished");
     }
 
